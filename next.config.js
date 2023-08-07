1b24eb6129a8dbe70b7c25ffa56c1e1788af1819
chiharuchiharuchiharu <x23060xx@aitech.ac.jp>
2023-08-07 17:22:53 +0900

    反映させた

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{
+      source: '/api/:path*',
+      destination: 'https://linebot-chi.onrender.com/api/:path*',
+    }]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,102 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [shifts, setShifts] = useState({});
+  const [selectDay, setSelectDay] = useState("");
+  const [selectRange, setSelectRange] = useState(["", ""]);
+
+  const omit = (str) => {
+    if (str.length <= 4) return str;
+    else return str.slice(0, 4) + "_";
+  };
+
+  function getDay(num) {
+    const day = ["月", "火", "水", "木", "金", "土", "日"];
+    return day[num];
+  }
+  async function fetchShifts(firstDay) {
+    if (!firstDay) return;
+    const url = `/api/shift/get?day=${firstDay}`;
+    return await fetch(url).then((res) => res.json());
+  }
+  function initSelectDay() {
+    const today = new Date();
+    const nextMonday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + ((7 - today.getDay()) % 7) + 1
+    );
+    const yyyy = nextMonday.getFullYear();
+    const mm = ("0" + (nextMonday.getMonth() + 1)).slice(-2);
+    const dd = ("0" + nextMonday.getDate()).slice(-2);
+    setSelectDay(`${yyyy}-${mm}-${dd}`);
+  }
+  function getSundays(day) {
+    const today = new Date(day);
+    const nextMonday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + ((7 - today.getDay()))
+    );
+    const mm = nextMonday.getMonth() + 1;
+    const dd = nextMonday.getDate();
+    return `${mm}/${dd}`;
+  }
+  function getMonday(day) {
+    const today = new Date(day);
+    const nextMonday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - today.getDay() + 1
+    );
+    const mm = nextMonday.getMonth() + 1;
+    const dd = nextMonday.getDate();
+    return `${mm}/${dd}`;
+  }
+  function setNextMonday() {
+    const today = new Date(selectDay);
+    const nextMonday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + ((7 - today.getDay()) % 7) + 1
+    );
+    const yyyy = nextMonday.getFullYear();
+    const mm = ("0" + (nextMonday.getMonth() + 1)).slice(-2);
+    const dd = ("0" + nextMonday.getDate()).slice(-2);
+    setSelectDay(`${yyyy}-${mm}-${dd}`);
+  }
+  function setPrevMonday() {
+    const today = new Date(selectDay);
+    const nextMonday = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - today.getDay() + 1 - 7
+    );
+    const yyyy = nextMonday.getFullYear();
+    const mm = ("0" + (nextMonday.getMonth() + 1)).slice(-2);
+    const dd = ("0" + nextMonday.getDate()).slice(-2);
+    setSelectDay(`${yyyy}-${mm}-${dd}`);
+  }
+  useEffect(() => {
+    initSelectDay();
+  }, []);
+  useEffect(() => {
+    const monday = getMonday(selectDay);
+    const sunday = getSundays(selectDay);
+    setSelectRange([monday, sunday]);
+
+    (async () => {
+      const s = await fetchShifts(selectDay);
+      console.log(s);
+      if (s) setShifts(s);
+    })();
+  }, [selectDay]);
   return (
     <>
       <Head>
@@ -14,101 +105,61 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className={inter.className}>
+        <header className={styles.header}>ヘッダー</header>
+
+        <div className={styles.ctrl_container}>
+          <div className={styles.last_week}>
+            <p onClick={setPrevMonday}>前週</p>
+          </div>
+
+          <div className={styles.range}>
+            {selectRange[0]}~{selectRange[1]}
+          </div>
+
+          <div className={styles.next_week}>
+            <p onClick={setNextMonday}>翌週</p>
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.gunchart_container}>
+          {Object.keys(shifts).map((key, i) => {
+            return (
+              <div className={styles.date_container} key={key}>
+                <div className={styles.date}>
+                  <div>
+                    <div>{getDay(i)}</div>
+                    <div>{key}</div>
+                  </div>
+                </div>
+                <div className={styles.names}>
+                  {shifts[key].data.map((data, i) => {
+                    return (
+                      <div className={styles.name} key={i}>
+                        <div>{omit(data.name)}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className={styles.guncharts}>
+                  {shifts[key].data.map((data, i) => {
+                    return (
+                      <div
+                        className={styles.bar}
+                        style={{ "--start": data.start, "--end": data.end }}
+                        key={i}
+                      ></div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </main>
+
+      <footer className={styles.footer}>
+        <p className={styles.copylight}>miroku © 2023 Copyright.</p>
+      </footer>
     </>
-  )
+  );
 }
